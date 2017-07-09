d609c2819fd71b64d48546d9d06374f474dd8803
Sridhar Kidambi <sridharkidambi@srinivasas-mbp.attlocal.net>
2017-07-09 16:57:50 -0500

    components and binding part 1

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
 
 @Component({
-     selector:"app-server",
+    //  selector:"app-server",
+    //  selector:"[app-server]",
+     selector:".app-server",
      templateUrl:"./server.component.html"
  }
 )
 export class servercomponent{
+age:number=34;
+sex:string="male";
+
+     getage(){
+    return this.age;
+    }
 
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',
+  selector: '[app-servers]',
   templateUrl: './servers.component.html',
+  // template:`
+  // <app-server></app-server><app-server></app-server>
+  // <h1>I am from template</h1>
+  // `,
   styleUrls: ['./servers.component.css']
+  // styles:[`
+  // p { color:dodgerblue;}
+  // `
+  // ]
 })
 export class ServersComponent implements OnInit {
 
